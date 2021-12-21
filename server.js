8083b210043cde97f597144f25e3266d7ae1e786
HannahWritesCode <44559340+HannahWritesCode@users.noreply.github.com>
2021-12-20 16:15:51 -0800

    Update build path

--- server.js
+++ server.js
@@ -53,7 +53,7 @@ async function getAccessToken() {
 */
 
 // serve static build to Heroku 
-app.use('/build',express.static(path.join(__dirname, 'build')));
+app.use(express.static(path.join(__dirname, 'build'))); 
 app.get('*', (req, res) => { 
   res.sendFile(path.join(__dirname + '/build/index.html')) 
 });
