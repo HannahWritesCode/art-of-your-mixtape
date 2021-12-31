33f458f4028088ff671c56883a8cd4ab46b27fae
HannahWritesCode <44559340+HannahWritesCode@users.noreply.github.com>
2021-12-30 18:22:29 -0800

    Update code serving build to heroku

--- server.js
+++ server.js
@@ -56,8 +56,10 @@ app.listen(port, () => console.log(`Listening on port ${port}`));
 
 // serve static build to Heroku 
 app.use(express.static(path.join(__dirname, './build'))); 
-app.get('*', (req, res) => { 
-  res.sendFile(path.join(__dirname + './build/index.html')) 
-});
+if(process.env.NODE_ENV === 'production'){
+  app.get('/*', (req, res) => { 
+    res.sendFile(path.join(__dirname + './build/index.html')) 
+  });
+}
 
 module.exports = app;
