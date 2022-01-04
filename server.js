4dc05c4f5fffc483b09487eb3efd30f744d132cf
HannahWritesCode <44559340+HannahWritesCode@users.noreply.github.com>
2022-01-04 14:19:58 -0800

    Update request handler

--- server.js
+++ server.js
@@ -57,7 +57,7 @@ app.listen(port, () => console.log(`Listening on port ${port}`));
 
 // serve static build to Heroku 
 app.use(express.static(path.join(__dirname, './build'))); 
-app.get('/*', (req, res) => { 
+app.get('*', (req, res) => { 
   res.sendFile(path.join(__dirname + './build/index.html')) 
 });
 
