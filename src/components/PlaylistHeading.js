72a65bef0e15b05ddabb5f9d9b60ef5728e53238
HannahWritesCode <44559340+HannahWritesCode@users.noreply.github.com>
2022-01-08 15:15:34 -0800

    Remove isEmpty call

--- src/components/PlaylistHeading.js
+++ src/components/PlaylistHeading.js
@@ -42,7 +42,7 @@ const PlaylistHeading = () => {
   }, []);
 
   useEffect(()=>{
-    if(!accessToken.isEmpty()){
+    if(accessToken){
       // GET Spotify Playlist object 
       const playlistName = axios.get(`https://api.spotify.com/v1/playlists/${playlist_id}`,{ headers:{'Authorization': `Bearer ${accessToken}`}});
       const playlistCover = axios.get(`https://api.spotify.com/v1/playlists/${playlist_id}/images`,{ headers:{'Authorization': `Bearer ${accessToken}`}});
