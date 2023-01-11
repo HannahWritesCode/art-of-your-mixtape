5130892b58ba9723b01deb5d4230ee563d65941b
Hannah Geer <44559340+HannahWritesCode@users.noreply.github.com>
2023-01-10 22:10:17 -0800

    Change dots to line chart with dummy data

--- client/src/components/charts/LineChart.js
+++ client/src/components/charts/LineChart.js
@@ -1,36 +1,41 @@
 import React from 'react';
 import { useRef, useEffect, useState } from 'react';
 //import ReactDOM from "react-dom";
-import { select } from 'd3';
+import { select, line, curveCardinal } from 'd3';
 
 //const data = [25, 30, 45, 60, 20]
 
 const LineChart = () => {
 
-    const [data, setData] = useState([25, 30, 45, 60, 20])
-
+    const [data, setData] = useState([25, 30, 45, 60, 20, 65, 75])
     const svgRef = useRef()
-    console.log(svgRef)
 
     useEffect(() => {
-        console.log(svgRef)
         const svg = select(svgRef.current)
+        const myLine = line()
+            .x((value, index) => index * 50)
+            .y(value => 150 - value)
+            .curve(curveCardinal);
         svg
-            .selectAll("circle")
-            .data(data)
-            .join("circle")
-            .attr("r", value => value)
-            .attr("cx", value => value * 2)
-            .attr("cy", value => value * 2)
-            .attr("stroke", "red")
+            .selectAll("path")
+            .data([data])
+            .join("path")
+            .attr("d", value => myLine(value))
+            .attr("fill", "none")
+            .attr("stroke", "blue");
     }, [data])
 
     return (
-        <div>
-            <svg ref={svgRef}>
-                <circle />
-            </svg>
-        </div>
+        <React.Fragment>
+            <svg ref={svgRef}></svg>
+            <br />
+            <button onClick={(() => setData(data.map(value => value + 5)))}>
+                Update Data
+            </button>
+            <button onClick={(() => setData(data.filter(value => value < 35)))}>
+                Filter Data
+            </button>
+        </React.Fragment>
     );
 
 
